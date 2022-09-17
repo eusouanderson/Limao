33d30b46f50aa9ef074712de367f83440bfdfb82
Anderson <55605767+eusouanderson@users.noreply.github.com>
2022-09-17 08:52:27 -0300

    Update api.py

--- Src/api.py
+++ Src/api.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ"ACbbafaef13a9f3d2eb06a20b2e96c7c15"
-auth_token = os.environ['5511954914441']
+account_sid = os.environ"xxxxxxxxx"
+auth_token = os.environ['xxxxxxxxx1']
 client = Client(account_sid, auth_token)
 
 message = client.messages('MM800f449d0399ed014aae2bcc0cc2f2ec').fetch()
